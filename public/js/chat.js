e7c9e71bad3e4a88e0e64ec389f557a53d2b42dd
marcosjrs <marcosjrs80@gmail.com>
2020-12-27 16:49:09 +0100

    Mostrar usuarios conectados

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,32 +1,50 @@
 //Usamos el objeto io, que nos viene dado del socket.io.js que lo general automaticamente el Socket.IO
-const socket = io(); // Hará que salte el listen de 'connection' en la parte Back
+var socket = io(); // Hará que salte el listen de 'connection' en la parte Back
+
+var params = new URLSearchParams(window.location.search);
+if (!params.has('nombre')) {
+    window.location = 'index.html';
+}
+var nombre = params.get('nombre');
+$("#lblNombreUsuario").text(nombre);
+
 //Elementos del DOM 
+
 
 //Ejemplo de emisión desde cliente
 socket.emit(
-    'aplicacion:eventoDelCliente',
-    {  },
-    function(infoPasadaPorServidor){
-        console.log('Respuesta Servidor: ',infoPasadaPorServidor);
+    'chat:peticionEntradaDeUsuarioEnChat',
+    { nombre: nombre },
+    function (infoPasadaPorServidor) {
+        console.log('Respuesta Servidor: ', infoPasadaPorServidor);
+       
+        let txtUsuarios = '';
+        var usuarios = infoPasadaPorServidor.usuarios;
+        for (let i = 0; i < usuarios.length; i++) {
+            txtUsuarios += '<li><p>'+usuarios[i].nombre+'</p></li>';
+            
+        }
+        $("#usuarios").html(txtUsuarios);
+        
     }
 );
 
 //Escuchamos conexion, desconexión y otros eventos del socket del servidor
-socket.on('connect', function(){
+socket.on('connect', function () {
     console.log('[cliente conectado]');
 });
-socket.on('disconnect', function(){
+socket.on('disconnect', function () {
     console.log('[cliente perdió conexión con servidor]');
 });
-socket.on('aplicacion:informacionDesdeServidor', function(informacion){
+socket.on('chat:informacionDesdeServidor', function (informacion) {
     console.log(informacion);
 });
 
 
 // Escuchamos el evento emitido por el servidor, que lo emite cuando se le envia un mensaje nuevo
 // desde algún cliente.
-socket.on('aplicacion:eventoParaQueActualiceEnTodosClientes', function(mensaje){
-   
+socket.on('chat:entradaDeUsuarioEnChat', function (mensaje) {
+
 });
 
 
--- servidor/sockets/socket.js
+++ servidor/sockets/socket.js
@@ -1,29 +1,35 @@
+const { Usuarios } = require('../classes/usuarios');
 const { io } = require('../index');
+
+let usuarios = new Usuarios();
 
 io.on('connection', (conexionDeUnCliente) => {
 
     console.log('[Nuevo cliente conectado]', conexionDeUnCliente.id);
     //Enviando eventos al cliente
-    conexionDeUnCliente.emit('aplicacion:informacionDesdeServidor', {
+    conexionDeUnCliente.emit('chat:informacionDesdeServidor', {
         version: '0.0.1',
-        texto: 'Bienvenido a esta aplicación'
+        texto: 'Bienvenido al chat'
     });
 
     //Escuchando eventos de cliente
-    conexionDeUnCliente.on('aplicacion:eventoDelCliente', (mensaje, callbackDeCliente) => {
-        //Enviamos el mensaje a todos los que están conectados a este servidor de socket
-        io.sockets.emit('aplicacion:eventoParaQueActualiceEnTodosClientes', mensaje);
-        //solo al resto, sería ¿? :
-        //conexionDeUnCliente.broadcast.emit('aplicacion:eventoParaQueActualiceEnTodosClientes', mensaje);
-        //LLamamos a la función que nos indica el cliente (se ejecutará en el cliente)
-        let ok = false;
-        if (mensaje.usuario && mensaje.mensaje) {
-            ok = true;
+    conexionDeUnCliente.on('chat:peticionEntradaDeUsuarioEnChat', (datosDesdeCliente, callbackDeCliente) => {
+        if (!datosDesdeCliente.nombre) {
+            callbackDeCliente({ ok: false, mensaje: 'Faltan parametros' });
+        } else {
+            //Devolveremos todos los usuarios de la "sala" (por ahora no está integrada la parte de las salas)
+            
+            usuarios.add(conexionDeUnCliente.id, datosDesdeCliente.nombre);
+            callbackDeCliente({ ok: true, usuario:datosDesdeCliente.nombre , usuarios: usuarios.getAll() });
+
+            //Enviamos el mensaje a todos los que están conectados a este servidor de socket
+            //conexionDeUnCliente.broadcast.emit('chat:entradaDeUsuarioEnChat', { });
         }
-        callbackDeCliente({ ok });
     });
-    conexionDeUnCliente.on('disconnect', (mensaje) => {
+
+    conexionDeUnCliente.on('disconnect', () => {
         console.log('[Cliente perdió o cerró la conexión con este servidor]');
+        usuarios.del(conexionDeUnCliente.id);
     });
 
 
