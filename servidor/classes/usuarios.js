081cab34d5bfa04f4a6bb113059148811868732c
marcosjrs <marcosjrs80@gmail.com>
2020-12-27 13:54:37 +0100

    Principio de clase manejadora de usuarios conectados

---
+++ servidor/classes/usuarios.js
@@ -0,0 +1,61 @@
+/**
+ * Clase donde se manejarán los usuarios conectados
+ */
+
+class Usuarios {
+
+    constructor() {
+        this.usuarios = []
+    }
+
+    /**
+     * Agrega un usuario
+     * @param {string} id 
+     * @param {string} nombre 
+     */
+    add(id, nombre) {
+        this.usuarios.push({ id, nombre });
+        return this.usuarios;
+    }
+
+    /**
+     * Elimina un usuario de la colección. 
+     * Devuelve los datos de la persona borrada (si existía).
+     * @param {string} id Id del usuario a eliminar
+     */
+    del(id) {
+        const personaBorrada = this.get(id)
+        this.usuarios = this.usuarios.filter(function(u){ return u.id !== id});
+        return personaBorrada;
+    }
+
+    /**
+     * Obtener el usuario que contenga el id pasado 
+     * por parametro
+     * @param {string} id Id del usuario a obtener
+     */
+    get(id) {
+        return this.usuarios.filter(function (u) { return u.id === id })[0];
+    }
+
+    /**
+     * Devuelve todos los usuarios
+     */
+    getAll(){
+        return this.usuarios;
+    }
+
+
+    /**
+     * Devuelve todos los usuario de una sala en concreto
+     * @param {*} sala 
+     */
+    getBySala(sala){
+        //TODO
+    }
+
+}
+
+module.exports = {
+    Usuarios
+}
